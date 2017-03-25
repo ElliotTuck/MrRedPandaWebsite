b24da52fef35b69c7fb9246119beeb150d4359d1
Elliot Tuck <eat3774@gmail.com>
2017-03-25 00:16:08 -0400

    Add site-wide fade-in on start and skills button slide toggle of mr red panda's skills

--- main.js
+++ main.js
@@ -1,39 +1,56 @@
-function getImage() {
-		var curPath = document.getElementById("switch").getAttribute("src");
-		if (curPath === "switch_images/switch7.JPG") {
-			path = "switch_images/switch1.jpeg";
-		} else if (curPath === "switch_images/switch1.jpeg") {
-			path = "switch_images/switch2.jpeg";
-		} else if (curPath === "switch_images/switch2.jpeg") {
-			path = "switch_images/switch3.jpeg";
-		} else if (curPath === "switch_images/switch3.jpeg") {
-			path = "switch_images/switch4.jpeg";
-		} else if (curPath === "switch_images/switch4.jpeg") {
-			path = "switch_images/switch5.jpeg";
-		} else if (curPath === "switch_images/switch5.jpeg") {
-			path = "switch_images/switch6.jpeg";
-		} else {
-			path = "switch_images/switch7.JPG"
-		}
-		document.getElementById("switch").src = path;
-}
+(function( $ ) {
+	'use strict';
 
-function getImageRev() {
-		var curPath = document.getElementById("switch").getAttribute("src");
-		if (curPath === "switch_images/switch2.jpeg") {
-			path = "switch_images/switch1.jpeg";
-		} else if (curPath === "switch_images/switch3.jpeg") {
-			path = "switch_images/switch2.jpeg";
-		} else if (curPath === "switch_images/switch4.jpeg") {
-			path = "switch_images/switch3.jpeg";
-		} else if (curPath === "switch_images/switch5.jpeg") {
-			path = "switch_images/switch4.jpeg";
-		} else if (curPath === "switch_images/switch6.jpeg") {
-			path = "switch_images/switch5.jpeg";
-		} else if (curPath === "switch_images/switch7.JPG") {
-			path = "switch_images/switch6.jpeg";
-		} else {
-			path = "switch_images/switch7.JPG"
+	$(function() {
+
+		$('body').hide();
+
+		$('body').fadeIn(1000);
+
+		$('.skills-btn').next().hide();
+
+		$('.skills-btn').on('click', function() {
+			$(this).next().slideToggle(400);
+		});
+
+		function getImage() {
+			var curPath = document.getElementById("switch").getAttribute("src");
+			if (curPath === "switch_images/switch7.JPG") {
+				path = "switch_images/switch1.jpeg";
+			} else if (curPath === "switch_images/switch1.jpeg") {
+				path = "switch_images/switch2.jpeg";
+			} else if (curPath === "switch_images/switch2.jpeg") {
+				path = "switch_images/switch3.jpeg";
+			} else if (curPath === "switch_images/switch3.jpeg") {
+				path = "switch_images/switch4.jpeg";
+			} else if (curPath === "switch_images/switch4.jpeg") {
+				path = "switch_images/switch5.jpeg";
+			} else if (curPath === "switch_images/switch5.jpeg") {
+				path = "switch_images/switch6.jpeg";
+			} else {
+				path = "switch_images/switch7.JPG"
+			}
+			document.getElementById("switch").src = path;
 		}
-		document.getElementById("switch").src = path;
-}
+
+		function getImageRev() {
+			var curPath = document.getElementById("switch").getAttribute("src");
+			if (curPath === "switch_images/switch2.jpeg") {
+				path = "switch_images/switch1.jpeg";
+			} else if (curPath === "switch_images/switch3.jpeg") {
+				path = "switch_images/switch2.jpeg";
+			} else if (curPath === "switch_images/switch4.jpeg") {
+				path = "switch_images/switch3.jpeg";
+			} else if (curPath === "switch_images/switch5.jpeg") {
+				path = "switch_images/switch4.jpeg";
+			} else if (curPath === "switch_images/switch6.jpeg") {
+				path = "switch_images/switch5.jpeg";
+			} else if (curPath === "switch_images/switch7.JPG") {
+				path = "switch_images/switch6.jpeg";
+			} else {
+				path = "switch_images/switch7.JPG"
+			}
+			document.getElementById("switch").src = path;
+		}
+	});
+})( jQuery );
